7fb9df83909b4a422c074cd56063f317a5311df6
LE VAN THANH <zero.lethanh@gmail.com>
2022-11-15 18:56:40 +0700

    add examples

---
+++ examples/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/zerolethanh/frlog"
+	"log"
+)
+
+func main() {
+	app := fiber.New()
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+	app.Post("/login", func(c *fiber.Ctx) error {
+		return c.SendString("Login")
+	})
+	app.Get("/user/:id", func(c *fiber.Ctx) error {
+		return c.SendString("User")
+	})
+	app.Patch("/fly/:from-:to", func(c *fiber.Ctx) error {
+		return c.SendString("Fly")
+	})
+
+	frlog.PrintAppStacks(app)
+
+	log.Fatalln(app.Listen(":3000"))
+}
